f9203ca64294c1fdd32ed02f2feb080df86341cf
MichaelAbe <michael.k.abe@gmail.com>
2020-12-08 14:59:15 -0500

    commit tree 5be51b6ac806799683a00791428266e257774fd8

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1, "Mr. Chocolate", 5, "M", "brown", "sweet", true);
+INSERT INTO bears VALUES (2, "Rowdy", 7, "F", "blonde", "mean", true);
+INSERT INTO bears VALUES (3, "Tabitha", 60, "F", "blonde", "witchy", false);
+INSERT INTO bears VALUES (4, "Sergeant Brown", 25, "M", "brown", "regimental", true);
+INSERT INTO bears VALUES (5, "Melissa", 30, "F", "black", "sweet", true);
+INSERT INTO bears VALUES (6, "Grinch", 32, "M", "green", "mean", false);
+INSERT INTO bears VALUES (7, "Wendy", 13, "F", "brown", "motherly", false);
+INSERT INTO bears VALUES (8, null, 15, "M", "brown", "mean", false);
